49ac4a69c7fe5095250d1ae7301b1a3d2954f177
35jakesmith <35jakesmith@gmail.com>
2022-02-11 17:51:48 -0700

    added swipe functionality and buttons

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+// import './App.css';
+import Header from './Componets/Header';
+import TinderCards from './Componets/TinderCards';
+import SwipeButtons from './Componets/SwipeButtons';
+
+function App() {
+  return (
+    <div className="App">
+    <Header />
+    <TinderCards />
+    <SwipeButtons />
+    </div>
+  );
+}
+
+export default App;
