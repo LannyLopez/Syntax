1d985869370a133cd12eff5250aae5c584e29f5b
Spoonska <Spencer.Kyle@hotmail.com>
2022-02-14 19:59:52 -0700

    fixed conflicts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,15 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'; // v5
-import Dashboard from './pages/Dashboard';
-import Login from './pages/Login';
+// import './App.css';
+import Header from './Componets/Header';
+import TinderCards from './Componets/TinderCards';
+import SwipeButtons from './Componets/SwipeButtons';
 
 function App() {
-  const [user, setUser] = useState({ token: '', author: null });
-  console.log(user);
   return (
-    <Router>
-      <div>
-        <header>
-          <h1>Library App</h1>
-          <ul>
-            <li>
-              <Link to="/">Dashboard</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <a href="https://www.google.com">Go to Google</a>
-            </li>
-          </ul>
-        </header>
-      </div>
-
-      <Switch>
-        <Route exact path="/" render={() => <Dashboard user={user} />} />
-        <Route exact path="/login" render={() => <Login setUser={setUser} />} />
-      </Switch>
-    </Router>
+    <div className="App">
+    <Header />
+    <TinderCards />
+    <SwipeButtons />
+    </div>
   );
 }
 
