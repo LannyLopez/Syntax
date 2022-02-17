0f5564cf2ba7ef97fca3d4aa27475a75ce16397d
Spoonska <Spencer.Kyle@hotmail.com>
2022-02-17 16:42:12 -0700

    fixed Users and Project mutations, added sign up front-end functionality

---
+++ client/src/Componets/GoogleSignIn.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { GoogleLogin } from 'react-google-login';
+
+const clientId = '922982704320-pdgv9aneald00po760gsp4qkds0fuvr0.apps.googleusercontent.com';
+
+function Login() {
+    const onSuccess = (res) => {
+        console.log('[Login Success] currentUser:', res);
+        
+    };
+
+    const onFailure = (res) => {
+        console.log('[Login failed] res:', res);
+    };
+
+    return (
+        <div>
+            <GoogleLogin
+                clientId={clientId}
+                buttonText="Login"
+                onSuccess={onSuccess}
+                onFailure={onFailure}
+                cookiePolicy={'single_host_origin'}
+                style={{ marginTop: '100px' }}
+                isSignedIn={true}
+            />
+        </div>
+    );
+}
+
+export default Login;
